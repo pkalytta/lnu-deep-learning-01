eeca1d7f70303965f8ce7afad6c21314c47f2360
Philipp Kalytta <philipp@kalytta.net>
2023-02-19 11:38:14 +0100

    * Fixed 01-leer

--- 02-keras/main_sec.py
+++ 02-keras/main_sec.py
@@ -6,7 +6,9 @@ from trainers import trainer_cnn
 
 
 def main():
-    pass
+    trainer = trainer_cnn.TrainerCNN(
+        epochs = 40, learning_rate = 0.001, val_split = 0.5, train_split = 0.5) # val_split
+    trainer.train()
 
 if __name__ == "__main__":
     main()
